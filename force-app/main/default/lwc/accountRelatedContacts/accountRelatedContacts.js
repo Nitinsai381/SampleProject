1a6680534b9bacadf09c32cc9153dd75a476d947
Nitinsai381 <nitinsai.leo33@gmail.com>
2024-09-25 19:26:48 +0530

    Optimized code

--- force-app/main/default/lwc/accountRelatedContacts/accountRelatedContacts.js
+++ force-app/main/default/lwc/accountRelatedContacts/accountRelatedContacts.js
@@ -3,20 +3,31 @@ import accountContacts from '@salesforce/apex/AccountRelatedContacts.relatedCont
 
 export default class accountRelatedContacts extends LightningElement {
     @api recordId;
-    contacts={};
+    contacts;
     // dynamicContacts={};
     labels=[];
     contactData = [];
     contactsArray = [];
+    slicedContactsArray ;
     contactKeys=[];
     notEmpty = false;
+    perSize = 5;
+    start = 0;
+    totalPages;
+    pageCount = 1;
     @wire(accountContacts, { accId: '$recordId' })
     wiredData({ data, error }){
         if (data) {
             console.log(data);
             console.log(typeof data);
             this.contacts = data;
-            this.notEmpty = this.contacts.length > 0;
+            // this.contacts =() => {
+            //     data.slice(this.start, this.start + this.perSize);
+            //    // console.log("data" + this.contacts);
+                
+            // };
+            
+            this.notEmpty = this.contacts[1].contacts.length > 0;
             console.log('Has contacts ===: ', this.notEmpty);
             console.log('this.contacts--', this.contacts);
             // this.contacts = data;
@@ -35,17 +46,17 @@ export default class accountRelatedContacts extends LightningElement {
         //        }}
             //    }
             // this.updateDisplayContacts();
-            this.labels = this.contacts.slice(0,data.length - 1);
+            this.labels = this.contacts[0].columns;
             console.log('Data in labels after slice ---- ', JSON.stringify(this.labels));
-            this.contactData = this.contacts.slice(this.contacts.length - 1, this.contacts.length);
+            this.contactData = this.contacts[1].contacts;
             console.log('Data in contacts after slice ---- ', JSON.stringify(this.contactData));
-            this.labels = this.labels.map(label => label.columnName);
+            this.labels = this.labels.map(label => label.Column__c);
             console.log('Data in labels ---- ', JSON.stringify(this.labels));
 
-            this.contactKeys = this.labels.map(column =>column.replace(' ', '.') );
+            // this.contactKeys = this.labels.map(column =>column.replace(' ', '.') );
                 
            
-            console.log('Keys in contacts from labels ===== ',JSON.stringify(this.contactKeys))
+            // console.log('Keys in contacts from labels ===== ',JSON.stringify(this.contactKeys))
             // this.contactKeys = Object.keys(this.contactKeys[0].contacts[0]);
             // console.log('Keys in contacts ===== ',JSON.stringify(this.contactKeys))
             // console.log('Data in contactData ---- ', JSON.stringify(this.contactData));
@@ -72,15 +83,29 @@ export default class accountRelatedContacts extends LightningElement {
             //     return [contact[key]];
             // });
             // })
-            
-            this.contactsArray = this.contactData[0].contacts.map(contact => {
-                return this.contactKeys.map(key => {
-                    
-                    return key.split('.').reduce((obj, prop) => obj && obj[prop], contact) || null;
-                });
-            });
-            console.log('Contacts in ContactsArray ==== ', JSON.stringify(this.contactsArray));
 
+            console.log('Contacts in contactData ==== ',JSON.stringify(this.contactData))
+            //VC
+            this.structuredData = this.contactData.map(contact =>{
+                let obj={};
+                obj.Id =contact.Id;
+                obj.record=this.buildRecord(contact);
+                return obj;
+            });
+            console.log('structuredData----', JSON.stringify(this.structuredData));
+            this.totalPages = Math.ceil(this.structuredData.length / this.perSize);
+
+       // - VC
+        
+//Mine==
+            // this.contactsArray = this.contactData[0].contacts.map(contact => {
+            //     return this.contactKeys.map(key => {
+                    
+            //         return key.split('.').reduce((obj, prop) => obj && obj[prop], contact) || null;
+            //     });
+            // });
+            // console.log('Contacts in ContactsArray ==== ', JSON.stringify(this.contactsArray));
+            this.updateDisplayContacts();
           
         }
         else if (error) {
@@ -90,35 +115,94 @@ export default class accountRelatedContacts extends LightningElement {
             console.log("Error in fetching data..")
         }
     }
-    // updateDisplayContacts() {
-    //     this.contacts = this.contacts.slice(this.start, this.start + this.perSize);
-    //     console.log("data"+ this.contacts);
+    //VC
+    buildRecord(contact){
+        let record=[];
+        this.labels.forEach(col => {
+            record.push(contact[col]);
         
-    // }
-    // onNext() {
+        });
+        return record;
+    }
+    //--VC
+
+    updateDisplayContacts() {
+        // this.slicedContactsArray = this.structuredData.slice(this.start, this.start + this.perSize);
+        this.slicedContactsArray = this.structuredData.slice(this.start, this.start + this.perSize);
+        console.log("Sliced structuredData for pagination -- "+ this.slicedContactsArray);
         
-    //     if (this.start + this.perSize < this.contacts.length) {
-    //         this.start += this.perSize;
-    //         this.updateDisplayContacts();
-    //         this.pageCount += 1
-    //         console.log('Total pages ---- ',Math.floor(this.contacts.length / this.perSize));
-    //     } else {
-    //         console.log("No more contacts to display.");
-    //     }
-    // }
-    // onPrevious() {
+    }
+    onNext() {
         
-    //     if (this.start - this.perSize >= 0) {
-    //         this.start -= this.perSize;
-    //         this.updateDisplayContacts();
-    //         this.pageCount-=1
-    //     } else {
-    //         console.log("No previous contacts to display.");
-    //     }
-    // }
+        if (this.start + this.perSize < this.structuredData.length) {
+            this.start += this.perSize;
+            this.updateDisplayContacts();
+            this.pageCount += 1
+            console.log('Total pages ---- ',Math.floor(this.structuredData.length / this.perSize));
+        } else {
+            console.log("No more contacts to display.");
+        }
+    }
+    onPrevious() {
+        
+        if (this.start - this.perSize >= 0) {
+            this.start -= this.perSize;
+            this.updateDisplayContacts();
+            this.pageCount-=1
+        } else {
+            console.log("No previous contacts to display.");
+        }
+    }
+    get isPreviousDisable() {
+        // let isDisable = true;
+        return (this.start === 0)
+            
+        //     isDisable = false;
+        // }
+        // return isDisable;
+    }
+    get isNextDisable() {
+        return ((this.structuredData.length - this.start) <= this.perSize)
+    }
 
 }
 
+// Varshith bro's clue code..
+// * a = [
+//     *  {label : "Name", ApiName : "Name"}
+//     *  {label : "Email", ApiName : "Email"}
+//     *  {label : "Phone", ApiName : "Phone"}
+//     * ]
+//     */
+   /**
+    * b = [
+    * {ID : "003aa", Name : "rec1", Email : "rec1@test.com", Phone : "123"}
+    * {ID : "003bb", Name : "rec2", Email : "rec2@test.com", Phone : "456"}
+    * {ID : "003cc", Name : "rec3", Email : "rec3@test.com", Phone : "678"}
+    * ]
+    *
+    * /[
+    *  {Id : "003", record : [values]}
+    * ]/
+    *  this.structuredData = b.map(e =>{
+    *      let obj = {};
+    *      obj.Id = e.Id;
+    *      obj.record = this.buildRecord(e);
+    *      return obj;
+    * });
+    *
+    * buildRecord(obj o){
+    *  [name, email, phome];
+    *  let x = a.map(e =>{
+    *      let y = [];
+    *      for( let col in e){
+    *          y.push(o[e.ApiName])
+    *      }
+    *      return y;
+    *  })
+    * return x;
+    * }
+    */
 
 
 
